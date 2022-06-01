0c8dc75f58c14d4878f5c9e1aef00aa431e76cb3
Scabby <61923737+Scabby@users.noreply.github.com>
2022-06-01 10:16:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// thanks, https://iquilezles.org/articles/warp/
-function warp(x, y, seed, octaves = 1) {
-    let x_warp = fractal(x + warp_rands[0], y + warp_rands[1], seed, octaves)
-    let y_warp = fractal(x + warp_rands[2], y + warp_rands[3], seed, octaves)
+function warp(x, y, seed, w_octaves = 1, f_octaves = 1) {
+    let x_warp = 0
+    let y_warp = 0
 
-    x_warp = fractal(x + x_warp * skew_scale + warp_rands[4], y + y_warp * skew_scale + warp_rands[5], seed, octaves)
-    y_warp = fractal(x + x_warp * skew_scale + warp_rands[6], y + y_warp * skew_scale + warp_rands[7], seed, octaves)
-
-    // I could extend this but I'm lazy
-
-    return fractal(x + x_warp * skew_scale, y + y_warp * skew_scale, seed, octaves)
+    for(let i = 0; i < w_octaves; i++) {
+        x_warp = fractal(
+            x + x_warp * skew_scale * generation_scale + warp_rands[i + 0],
+            y + y_warp * skew_scale * generation_scale + warp_rands[i + 1],
+            seed,
+            f_octaves
+        )
+        y_warp = fractal(
+            x + x_warp * skew_scale * generation_scale + warp_rands[i + 2],
+            y + y_warp * skew_scale * generatoin_scale + warp_rands[i + 3],
+            seed,
+            f_octaves
+        )
+    }
+    
+    return fractal(
+        x + x_warp * skew_scale * generation_scale,
+        y + y_warp * skew_scale * generation_scale,
+        seed,
+        f_octaves
+    )
 }
 
 function fractal(x, y, seed, octaves = 1) {
     let value = 0
 
     for(let i = 1; i <= octaves; i++) {
-        const scale     = (i * i) / fundamental_scale
+        const scale     = (i * i) / generation_scale
         const weight    = 1 / (i * i) // maximum precision is 16 because of this
 
         noise.seed(i)
@@ -35,7 +49,7 @@ async function draw() {
     for(let y = 0; y < height; y++) {
         for(let x = 0; x < width; x++) {
             const img = ctx.createImageData(1, 1)
-            const val = convert(warp(x, y, seed, precision))
+            const val = convert(warp(x, y, seed, warp_octaves, fractal_octaves))
             img.data[0] = val
             img.data[1] = val
             img.data[2] = val
@@ -60,12 +74,13 @@ window.onload = () => {
 
 width               = 250
 height              = 250
-fundamental_scale   = 100
+generation_scale    = 100
 skew_scale          = 30
-precision           = 8
+warp_octaves        = 5
+fractal_octaves     = 8
 seed                = parseInt(Math.random() * 65536)
 
 warp_rands = []
-for(i = 0; i < 8; i ++) {
+for(i = 0; i < 64; i ++) {
     warp_rands[i] = ((Math.random() - 0.5) * 2) * 10
 }
